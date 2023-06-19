a3cd79e9cda3c460e2824526c39dff521a80bc4c
Timothy Karani <kartimothy@gmail.com>
2023-06-20 01:15:29 +0300

    displaying routes

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
+        vue({
+            input: ["./resources/js/**/*.vue"],
+            refresh: true,
+        }),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
