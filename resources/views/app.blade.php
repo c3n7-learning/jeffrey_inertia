99eb19215038eae91aa1cca2bb24f28f0b5ab8bf
Timothy Karani <kartimothy@gmail.com>
2023-06-20 09:32:17 +0300

    Basic styling

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html theme="autumn">
 
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
   @vite('resources/js/app.js')
+  @vite('resources/css/app.css')
   @inertiaHead
 </head>
 
